270632ba7b30c7d22e62856185564929ba49a808
trickster1o1 <nischaltuladhar15@gmail.com>
2023-09-27 12:43:56 +0545

    popular component setup

---
+++ resources/views/Frontend/pages/components/popular.blade.php
@@ -0,0 +1,67 @@
+<div class="custom-cont">
+    <h3 class="custom-head">Popular</h3>
+    <div class="popular-cont">
+        <div>
+            <div class="t-thumb">
+                <img class="i-fit" src="https://img.grouponcdn.com/deal/ddPzt2ANHPaiTDHemkY3/1i-984x591/v1/c349x211.webp"
+                    alt="404">
+            </div>
+            <div class="t-body">
+                <span class="t-head">King Spa and Sauna</span>
+                <span>809 Civic Center Dr., Niles • 13.4 mi</span>
+                <span>4.8 <i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i
+                        class="far fa-star"></i><i class="far fa-star"></i> 50640 Ratings</span>
+                <span>$50 Discount price $35 $35 30% OFF</span>
+                <span>General Spa Admission for One</span>
+            </div>
+        </div>
+        <div>
+            <div class="t-thumb">
+                <img class="i-fit"
+                    src="https://img.grouponcdn.com/deal/sRBhS1YUqxKGFNoU6o6YGT/IMAGE_Valvoline-Instant-Oil-Change-Kansas-City_MO-700x420/v1/c349x211.webp"
+                    alt="404">
+            </div>
+
+            <div class="t-body">
+                <span class="t-head">King Spa and Sauna</span>
+                <span>809 Civic Center Dr., Niles • 13.4 mi</span>
+                <span>4.8 <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i
+                        class="fas fa-star"></i><i class="fas fa-star"></i> 50640 Ratings</span>
+                <span>$50 Discount price $35 $35 30% OFF</span>
+                <span>General Spa Admission for One</span>
+            </div>
+        </div>
+        <div>
+            <div class="t-thumb">
+                <img class="i-fit"
+                    src="https://img.grouponcdn.com/deal/3h3TjQREMspGsad7uFiFXUeCxGhv/3h-751x450/v1/c349x211.webp"
+                    alt="404">
+            </div>
+
+            <div class="t-body">
+                <span class="t-head">King Spa and Sauna</span>
+                <span>809 Civic Center Dr., Niles • 13.4 mi</span>
+                <span>4.8 <i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i
+                        class="far fa-star"></i><i class="far fa-star"></i> 50640 Ratings</span>
+                <span>$50 Discount price $35 $35 30% OFF</span>
+                <span>General Spa Admission for One</span>
+            </div>
+        </div>
+        <div>
+            <div class="t-thumb">
+                <img class="i-fit"
+                    src="https://img.grouponcdn.com/deal/sRBhS1YUqxKGFNoU6o6YGT/IMAGE_Valvoline-Instant-Oil-Change-Kansas-City_MO-700x420/v1/c349x211.webp"
+                    alt="404">
+            </div>
+
+            <div class="t-body">
+                <span class="t-head">King Spa and Sauna</span>
+                <span>809 Civic Center Dr., Niles • 13.4 mi</span>
+                <span>4.8 <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i
+                        class="fas fa-star"></i><i class="fas fa-star"></i> 50640 Ratings</span>
+                <span>$50 Discount price $35 $35 30% OFF</span>
+                <span>General Spa Admission for One</span>
+            </div>
+        </div>
+    </div>
+</div>
